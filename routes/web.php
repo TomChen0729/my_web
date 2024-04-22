148492cef2e427b6a9df5436ed15d9c95e9d0350
TomChen0729 <tom98075@gmail.com>
2024-04-22 14:23:41 +0800

    開始撰寫後端

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GameController;
 use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,13 +12,15 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|s
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 // Route::resource('/link', LinkController::class);
-Route::get('/links', function () {
-    return view('layouts.links');
-});
+// Route::get('/links', function () {
+//     return view('layouts.links');
+// });
+Route::resource('Games', GameController::class);
+Route::get('/country/{countryID}/Game/{GameMetod}', [GameController::class, '']);
